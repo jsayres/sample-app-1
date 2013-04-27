1aa7b4ea8a47475be8de599c3d9fd467d8c8317c
Jason Sayres <j.c.sayres@gmail.com>
2013-04-26 18:47:31 -0700

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { email.downcase! }
+  before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@(?:[a-z\d\-]+\.)+[a-z]+\z/i
@@ -8,4 +9,10 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -66,7 +66,9 @@ describe 'User pages' do
 
       describe "after saving the user" do
         before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
 
+        it { should have_link('Sign out') }
         it { should have_content(name) }
         it { should have_title(name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
